0f547e80187d7bec1810cc1a5d319550f2ceec77
glendex <glendasp@gmail.com>
2016-03-01 18:16:16 -0600

    updates

--- routes/index.js
+++ routes/index.js
@@ -14,23 +14,25 @@ router.get('/convert', convert);
 function convert(req, res){
 
     //Data send as a post request is available in the
-    //body.query attribute; propories same as name in form
+    //body.query attribute; properties same as name in form
 
-    var dollars = req.query.dollaramount;
+    var money = req.query.fromamount;
+    var convertFrom = req.query.fromcurrency;
     var convertTo = req.query.tocurrency;
 
+    console.log("query was: convert " + money + " to " + convertTo);
 
-    console.log("query was: convert " + dollars + " to " + convertTo);
-
-    //Our conversion rates - TODO what's wrong with this?
-    var conversion = { "Pound": 1.6, "Euro": 1.1};
+    //Our conversion rates - TODO what's wrong with this? It is only current for a specific day
+    var conversion = {"Pound": 1.6, "Euro": 1.1, "Dollars": 1.40};
 
     var conversionRate = conversion[convertTo];
 
     var convertedVal = conversionRate * dollars;
 
-    res.render('result', {dollars: dollars, currency:convertTo, converted:convertedVal});
-
+    res.render('result', {
+        start: money,
+        currency:convertTo,
+        converted:convertedVal});
 }
 
 module.exports = router;
